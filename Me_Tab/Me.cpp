bf1a318897d881fc1a59251f09c4635a33b05f2d
f8477 <162903214+f8477@users.noreply.github.com>
2024-07-07 18:18:56 +0330

    create template of profile(me) tab and fill the blanks with default values

--- Me_Tab/Me.cpp
+++ Me_Tab/Me.cpp
@@ -6,6 +6,15 @@ Me::Me(QWidget *parent) :
     ui(new Ui::Me)
 {
     ui->setupUi(this);
+
+    ui->nameOfUser->setText("Sajjad");
+    ui->lastnameOfUser->setText("Rahimi Shandiz");
+    ui->skillsOfUser->setPlainText("software engineer");
+    ui->skillsOfUser->setReadOnly(true);
+    ui->BioOfUser->setPlainText("EE Student at FUM | Electronic Designer | Linux Lover \nLove swim and trip");
+    ui->BioOfUser->setReadOnly(true);
+
+    ui->accountProfile->setPixmap(QPixmap(":/images/assets/profile.png"));
 }
 
 Me::~Me()
